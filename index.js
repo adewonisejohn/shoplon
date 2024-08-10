ecedb3bfb2c2e57274abfe4a0cc566f10123f293
Adewonise John <adewonisekayode@gmail.com>
2024-08-10 18:40:42 +0100

    this commit changes port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Product = require('./models/product');  // Assuming the model is in the mo
 
 const app = express();
 require("dotenv").config()
-const port = 3000;
+const port = process.env.PORT || 3000 ;
 
 
 console.log(process.env.MONGODB_URL)
